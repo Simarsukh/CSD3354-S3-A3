9816df99fa048f06dcca048179c86f3904d813f3
Simardeep Kaur <739292@student.tor.loc>
2019-07-10 09:17:53 -0400

    2nd commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -6,7 +11,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tp = new TextProcessing();
+            tp.Run();
         }
     }
+
+    class TextProcessing
+    {
+        public void Run()
+        {
+            // Open the names file and access the data
+            using (StreamReader file = new StreamReader("U:/Users/739292/Assignment_3/ConsoleApp1.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+    }
+
 }
